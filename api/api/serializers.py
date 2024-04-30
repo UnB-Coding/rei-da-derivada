64328215212a1cc326e1839ad3e48857488c5d06
GabrielCastelo-31 <gabflubr@outlook.com>
2024-04-30 20:40:24 -0300

    update serializers

--- api/api/serializers.py
+++ api/api/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
-from api.models import Token, Event
+from api.models import Token, Event, Sumula, PlayerScore, PlayerTotalScore
+from users.models import User
+
+
+class UserSerializer(ModelSerializer):
+    """ Serializer for the User model.
+    fields: first_name, last_name, uuid
+    """
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'uuid']
 
 
 class TokenSerializer(ModelSerializer):
@@ -8,7 +18,30 @@ class TokenSerializer(ModelSerializer):
         model = Token
         fields = ['token_code']
 
+
 class EventSerializer(ModelSerializer):
     class Meta:
         model = Event
-        fields = ['name']
+        fields = ['name', 'id']
+
+
+class PlayerTotalScoreSerializer(ModelSerializer):
+    class Meta:
+        model = PlayerTotalScore
+        fields = '__all__'
+
+
+class PlayerScoreSerializer(ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = PlayerScore
+        fields = ['user', 'event', 'sumula', 'points']
+
+
+class SumulaSerializer(ModelSerializer):
+    players_score = PlayerScoreSerializer(source='playerscore_set', many=True)
+
+    class Meta:
+        model = Sumula
+        fields = ['id', 'name', 'players_score']
