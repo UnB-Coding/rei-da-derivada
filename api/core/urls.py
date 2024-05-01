ebaca2d1270fb73956584a35234f7896ebfc5748
GabrielCastelo-31 <gabflubr@outlook.com>
2024-05-01 19:14:19 -0300

    Add swagger documentation to api

--- api/core/urls.py
+++ api/core/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Rei e Rainha da Derivada - API",
+        default_version='v1',
+        description="API para o projeto Aplicação Mobile para apoio ao Rei e Rainha da Derivada",
+        contact=openapi.Contact(email="reidaderivada2024@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     # Admin
@@ -25,4 +39,10 @@ urlpatterns = [
     # Views
     path('users/', include('users.urls')),
     path('api/', include('api.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 ]
