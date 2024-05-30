a5c1f5265144908b25c648297587ccff396b9dad
artrsousa1 <artrsousa1@gmail.com>
2024-05-30 12:47:43 -0300

    fix(navbar): creating position map for navbar

--- web/src/app/components/NavBarComponent.jsx
+++ web/src/app/components/NavBarComponent.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import homefill from '@/app/assets/home_fill.png';
+import homeline from '@/app/assets/home_line.png';
+import calendarfill from '@/app/assets/calendar_fill.png';
 import calendarline from '@/app/assets/calendar_line.png';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 const NavBarComponent = () => {
+    const routpath = usePathname();
+    const barPosition = {
+        "/home": "left-10",
+        "/event": "right-10",
+    };
+
     return (
-        <div className="bg-secondary w-full items-center justify-between px-20 sm:px-30 lg:px-1/4 xl:px-1/5 h-28 fixed bottom-0 flex">
-            <div className="bg-primary w-[124px] h-[6px] rounded-full absolute top-0 left-10"/>
-            <Image onClick={() => {console.log("deubom")}} src={homefill} alt="home icons" width={42} height={42} />
-            <Image onClick={() => {console.log("deubom")}} src={calendarline} alt="calendar icons" width={42} height={42}/>
+        console.log(usePathname()),
+        <div className="bg-secondary w-full items-center justify-between px-20 sm:px-30 lg:px-1/4 xl:px-1/5 h-20 fixed bottom-0 flex">
+            <div className={`bg-primary w-[124px] h-[6px] rounded-full absolute top-0 transition-all ${barPosition[routpath]}`}/>
+            <Image onClick={() => {console.log("deubom")}} src={homefill} alt="home icons" width={36} height={36} />
+            <Image onClick={() => {console.log("deubom")}} src={calendarline} alt="calendar icons" width={36} height={36}/>
       </div>
     );
 };
