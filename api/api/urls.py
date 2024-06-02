198674d1d3c53b5f4207f175c03e5a04a66679fc
GabrielCastelo-31 <gabflubr@outlook.com>
2024-06-02 18:29:28 -0300

    update urls to match new files

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, re_path
-from .views.views_event import TokenView, EventView, StaffView, AddStaffManager
+from .views.views_event import TokenView, EventView
+from .views.views_staff import StaffView, AddStaffManager, AddStaffMembers
 from .views.views_players import PlayersView, GetPlayerResults, AddPlayers, PublishPlayersResults, Top4Players
 from .views.views_sumulas import SumulaView, ActiveSumulaView, FinishedSumulaView
 
@@ -18,7 +19,8 @@ urlpatterns = [
     path('player/', GetPlayerResults.as_view(), name='player'),
     path('staff/', StaffView.as_view(), name='staff'),
     path('staff-manager', AddStaffManager.as_view(), name='staff-manager'),
-    path('upload/', AddPlayers.as_view(), name='upload'),
+    path('upload-players/', AddPlayers.as_view(), name='upload'),
+    path('upload-staff/', AddStaffMembers.as_view(), name='upload-staff'),
     path('top4/', Top4Players.as_view(), name='top4'),
     path('publish-results/', PublishPlayersResults.as_view(), name='publish-results'),
 ]
