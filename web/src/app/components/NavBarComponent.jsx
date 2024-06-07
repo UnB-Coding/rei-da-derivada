12e3ef9a69969b07ccc00019c6a5cea7aab85e68
artrsousa1 <artrsousa1@gmail.com>
2024-06-07 17:26:39 -0300

    fix: changing the fill property of the icons

--- web/src/app/components/NavBarComponent.jsx
+++ web/src/app/components/NavBarComponent.jsx
@@ -11,16 +11,16 @@ const NavBarComponent = () => {
     const router = useRouter();
     const routpath = usePathname();
     const barPosition = {
-        "/home": "left-10",
-        "/contests": "right-10",
+        "/home": "left-11",
+        "/contests": "right-11",
     };
 
     return (
-        <div className="bg-secondary w-full items-center justify-around px-20 sm:px-30 lg:px-1/4 xl:px-1/5 h-20 fixed bottom-0 flex">
-            <div className={`bg-primary w-[124px] h-[6px] rounded-full absolute top-0 transition-all ${barPosition[routpath]}` }/>
-            <Image onClick={() => {router.push("/home")}} src={homefill} alt="home icons" width={36} height={36} />
-            <Image onClick={() => {router.push("/contests")}} src={calendarline} alt="calendar icons" width={36} height={36}/>
-      </div>
+        <div className="bg-secondary w-full items-center justify-around  sm:px-30 lg:px-1/4 xl:px-1/5 h-20 fixed bottom-0 flex">
+            <div className={`bg-primary w-[124px] h-[6px] rounded-full absolute top-0  ${barPosition[routpath]}`}/>
+            <Image onClick={() => {router.push("/home")}} src={routpath == '/home' ? homefill : homeline} alt="home icons" width={36} height={36} />
+            <Image onClick={() => {router.push("/contests")}} src={routpath == '/contests' ? calendarfill : calendarline} alt="calendar icons" width={36} height={36}/>
+        </div>
     );
 };
 
