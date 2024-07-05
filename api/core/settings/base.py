c728a8e33f7546660c76be4b3b0291044be98f4c
GabrielCastelo-31 <gabflubr@outlook.com>
2024-07-04 22:48:25 -0300

    disable de csrf token temporarily

--- api/core/settings/base.py
+++ api/core/settings/base.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'api.views.base_views.DisableCSRFMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
