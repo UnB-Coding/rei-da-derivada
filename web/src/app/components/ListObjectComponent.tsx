4c2b318317498fd56cb8458d43b38fbb289ccc1b
artrsousa1 <artrsousa1@gmail.com>
2024-07-16 22:57:14 -0300

    feat(front): consuming events routes

--- web/src/app/components/ListObjectComponent.tsx
+++ web/src/app/components/ListObjectComponent.tsx
@@ -1,19 +1,59 @@
 import React from "react";
 import JoinBoxComponent from "./JoinBoxComponent";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
+interface UserEvent {
+    role?: string;
+    event?: {
+      id: number;
+      name: string;
+      active: boolean;
+    }
+}
 interface ListObjectComponentProps {
     title: string;
-    active?: boolean;
+    live?: boolean;
 }
 
 const ListObjectComponent = (props: ListObjectComponentProps) => {
+    const { user } = useContext(UserContext);
+    const events = user.all_events;
+    const activeEvents: UserEvent[] = [];
+    const endedEvents: UserEvent[] = [];
+    let hasEvents = false;
+
+    if (events !== undefined && events.length > 0) {
+        hasEvents = true;
+        events.forEach((event) => {
+            if(event.event?.active){
+                activeEvents.push(event);
+            } else {
+                endedEvents.push(event);
+            }
+        })
+    }
     return(
         <div className="grid justify-center items-center gap-4 pb-4 pt-10">
             <p className="text-xl font-semibold text-primary ">{props.title.toUpperCase()}</p>
-            <JoinBoxComponent title="unb-rrdd-24" active={props.active}/>
-            <JoinBoxComponent title="unb-rrdd-25" active={props.active}/>
-            <JoinBoxComponent title="unb-rrdd-26" active={props.active}/>
-            <JoinBoxComponent title="unb-rrdd-27" active={props.active}/>
+            {props.live ? 
+            <>
+                {!hasEvents || activeEvents.length === 0 ? <p>Nenhum evento contrado</p>
+                : activeEvents.map((event) => {
+                    return (
+                        <JoinBoxComponent name={event.event?.name} active={event.event?.active}/>
+                    )
+                })}
+            </>
+            :
+            <>
+                {!hasEvents || endedEvents.length === 0 ? <p>Nenhum evento contrado</p>
+                : endedEvents.map((event) => {
+                    return (
+                        <JoinBoxComponent name={event.event?.name} active={event.event?.active}/>
+                    )
+                })}
+            </>}
         </div>
     );
 }
--- web/src/app/utils/api/getEvents.ts
+++ web/src/app/utils/api/getEvents.ts
@@ -1,8 +1,12 @@
 import request from "../request";
 import { settingsWithAuth } from "../settingsWithAuth";
 
-export default async function getEvents(access_token?: string) {
+export default async function getEvents(access_token?: string){
     const response = await request.get("/api/event/",(settingsWithAuth(access_token)));
-
-    return response;
+    if(response.status === 200){
+        const data = response.data;
+        return data;
+    } else {
+        return [];
+    }
 }
