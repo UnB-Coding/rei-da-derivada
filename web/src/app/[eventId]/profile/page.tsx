f709dfe5af1f91018c6946c65dd9795f6ebe64a3
artrsousa1 <artrsousa1@gmail.com>
2024-07-27 23:43:54 -0300

    validando rotas nas páginas de evento

--- web/src/app/[eventId]/profile/page.tsx
+++ web/src/app/[eventId]/profile/page.tsx
@@ -1,22 +1,41 @@
 'use client'
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "@/app/contexts/UserContext";
-import { useRouter } from "next/navigation";
+import { EventContext } from "@/app/contexts/EventContext";
+import { useRouter, usePathname } from "next/navigation";
 import HeaderComponent from "@/app/components/HeaderComponent";
 import LoadingComponent from "@/app/components/LoadingComponent";
 import EventNavBarComponent from "@/app/components/EventNavBarComponent";
+import validatePath from "@/app/utils/validadePath";
+import getBasePath from "@/app/utils/getBasePath";
 
 export default function Profile() {
   const { user, loading } = useContext(UserContext);
+  const [ canSee, setCanSee ] = useState<boolean>(false);
   const router = useRouter();
+  const params = usePathname().split("/");
+  const currentId = parseInt(params[1]);
+  const currentPath = params[2];
+
   useEffect(() => {
     if (!user.access && !loading) {
-        //Adicionar um verificador para saber se o evento existe
-        router.push("/");
+      router.push("/");
+    } else if (user.all_events) {
+      const current = user.all_events.find(elem => elem.event?.id === currentId);
+      if (current && current.role) {
+        validatePath(current.role,currentPath) === true ?
+        setCanSee(true) : router.push(`/${currentId}/${getBasePath(current.role)}`);
+      } else {
+        router.push("/contests");
+      }
     }
   }, [user]);
 
-  return loading ? <LoadingComponent/> :
+  if(!canSee || loading){
+    return <LoadingComponent/>;
+  }
+
+  return(
     <>
       <HeaderComponent/>
       <div className="grid justify-center items-center py-32 px-2 text-center">
@@ -25,6 +44,5 @@ export default function Profile() {
       </div>
       <EventNavBarComponent/>
     </>
-    
-  
+  );
 }
--- web/src/app/[eventId]/sumula/page.tsx
+++ web/src/app/[eventId]/sumula/page.tsx
@@ -1,27 +1,46 @@
 'use client'
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "@/app/contexts/UserContext";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
+import { Toast, toast } from "react-hot-toast";
 import LoadingComponent from "@/app/components/LoadingComponent";
 import HeaderComponent from "@/app/components/HeaderComponent";
 import ActiveSumulaComponent from "@/app/components/ActiveSumulaComponent";
 import EventNavBarComponent from "@/app/components/EventNavBarComponent";
+import validatePath from "@/app/utils/validadePath";
+import getBasePath from "@/app/utils/getBasePath";
 
 export default function Sumula() {
   const { user, loading } = useContext(UserContext);
+  const [ canSee, setCanSee ] = useState<boolean>(false);
   const router = useRouter();
+  const params = usePathname().split("/");
+  const currentId = parseInt(params[1]);
+  const currentPath = params[2];
+
   useEffect(() => {
     if (!user.access && !loading) {
-        //Adicionar um verificador para saber se o evento existe
-        //Adicionar um verificador para saber se o usuário é monitor
-        router.push("/");
+      router.push("/");
+    } else if (user.all_events) {
+      const current = user.all_events.find(elem => elem.event?.id === currentId);
+      if (current && current.role) {
+        validatePath(current.role,currentPath) === true ?
+          setCanSee(true) : router.push(`/${currentId}/${getBasePath(current.role)}`);
+      } else {
+        router.push("/contests");
+      }
     }
   }, [user]);
 
-  return loading ? <LoadingComponent/>:
+  if(!canSee || loading){
+    return <LoadingComponent/>;
+  }
+
+  return( 
     <>
         <HeaderComponent/>
         <ActiveSumulaComponent/>
         <EventNavBarComponent/>
     </>
+  );
 }
