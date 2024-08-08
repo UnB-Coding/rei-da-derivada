13fa47dfb9c073a40401a1b7d2248ae556c00658
GabrielCastelo-31 <gabflubr@outlook.com>
2024-08-07 22:39:42 -0300

    add round robin prototype

--- api/api/views/base_views.py
+++ api/api/views/base_views.py
@@ -2,6 +2,8 @@ from django.utils.deprecation import MiddlewareMixin
 from django.forms import ValidationError
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
+
+from ..serializers import PlayerScoreSerializer
 from ..models import Event, PlayerScore, Staff, SumulaImortal, SumulaClassificatoria, Player
 
 EVENT_NOT_FOUND_ERROR_MESSAGE = "Evento não encontrado!"
@@ -29,6 +31,96 @@ class BaseView(APIView):
 
 class BaseSumulaView(BaseView):
     """Classe base para as views de sumula. Contém métodos comuns a todas as views de sumula."""
+
+    def round_robin_tournament(self, n: int, players_score: list[PlayerScore]) -> list[list[tuple[PlayerScore, int]]]:
+        """Gera os pares de jogadores para um torneio com n-1 rodadas.
+        Todos os jogadores jogam com todos os outros jogadores em formato de duplas"""
+        if len(players_score) != n:
+            raise Exception(
+                "Número de jogadores não corresponde ao número fornecido!")
+
+        if n % 2 != 0:
+            players_score.append(None)
+            n += 1
+
+        ordem = {
+            4: [
+                [(1, 2), (3, 4)],
+                [(1, 3), (2, 4)],
+                [(1, 4), (2, 3)]
+            ],
+            5: [
+                [(1, 2), (3, 4)],
+                [(1, 3), (2, 5)],
+                [(1, 4), (2, 3)],
+                [(1, 5), (2, 4)],
+                [(1, 3), (4, 5)]
+            ],
+            6: [
+                [(1, 2), (3, 4), (5, 6)],
+                [(1, 3), (2, 5), (4, 6)],
+                [(1, 4), (2, 3), (5, 6)],
+                [(1, 5), (2, 4), (3, 6)],
+                [(1, 6), (2, 5), (3, 4)]
+            ],
+            7: [
+                [(1, 2), (3, 4), (5, 6)],
+                [(1, 3), (2, 5), (4, 7)],
+                [(1, 4), (2, 3), (5, 7)],
+                [(1, 5), (2, 4), (3, 6)],
+                [(1, 6), (2, 5), (3, 7)],
+                [(1, 7), (2, 6), (3, 4)]
+            ],
+            8: [
+                [(1, 2), (3, 4), (5, 6), (7, 8)],
+                [(1, 3), (2, 4), (5, 7), (6, 8)],
+                [(1, 4), (2, 3), (5, 8), (6, 7)],
+                [(1, 5), (2, 6), (3, 7), (4, 8)],
+                [(1, 6), (2, 5), (3, 8), (4, 7)],
+                [(1, 7), (2, 8), (3, 5), (4, 6)],
+                [(1, 8), (2, 7), (3, 6), (4, 5)]
+            ]
+        }
+
+        if n not in ordem:
+            raise Exception(
+                "Número de jogadores não suportado no dicionário de ordem!")
+
+        # Mapear os jogadores para os seus respectivos números
+        player_map = {i + 1: players_score[i] for i in range(n)}
+
+        # Gerar os pares de jogadores conforme a ordem predefinida
+        numbered_rounds = []
+        for round_pairs in ordem[n]:
+            numbered_round = []
+            for p1, p2 in round_pairs:
+                player1 = player_map[p1]
+                player2 = player_map[p2] if p2 <= len(players_score) else None
+                if player1 is not None:
+                    player1.rounds_number = p1
+                if player2 is not None:
+                    player2.rounds_number = p2
+                numbered_round.append((player1, player2))
+            numbered_rounds.append(numbered_round)
+
+        # Caso o número de jogadores seja ímpar (5 ou 7), garantir que um jogador jogue sozinho a cada rodada
+        if len(players_score) % 2 != 0:
+            for i, round in enumerate(numbered_rounds):
+                for j, pair in enumerate(round):
+                    if pair[1] is None:
+                        player1 = pair[0]
+                        player1.rounds_number = ordem[n][i][j][0]
+                        numbered_rounds[i][j] = (player1, None)
+        for s, round in enumerate(numbered_rounds):
+            print('ROUND:', s+1)
+            for pair in round:
+                if pair[1] is not None:
+                    print(
+                        pair[0].rounds_number, pair[1].rounds_number)
+                else:
+                    print(pair[0].rounds_number, "joga sozinho")
+
+        return numbered_rounds
 
     def validate_request_data_dict(self, data):
         """Valida se os dados fornecidos na requisição estão no formato correto."""
@@ -82,8 +174,9 @@ class BaseSumulaView(BaseView):
                 event=event, active=active)
         return sumula_imortal, sumula_classificatoria
 
-    def create_players_score(self, players: list, sumula: SumulaImortal | SumulaClassificatoria, event: Event,) -> None | ValidationError:
+    def create_players_score(self, players: list, sumula: SumulaImortal | SumulaClassificatoria, event: Event,) -> list[PlayerScore] | ValidationError:
         """Cria uma lista de PlayerScore associados a uma sumula."""
+        players_score = []
         for player in players:
             player_id = player.get('id')
             if player_id is None:
@@ -93,11 +186,13 @@ class BaseSumulaView(BaseView):
                 raise ValidationError(
                     f"Jogador {player.get('name')} não encontrado!")
             if sumula.__class__ == SumulaImortal:
-                PlayerScore.objects.create(
-                    player=player_obj, sumula_imortal=sumula, event=event)
+                players_score.append(PlayerScore.objects.create(
+                    player=player_obj, sumula_imortal=sumula, event=event))
             else:
-                PlayerScore.objects.create(
-                    player=player_obj, sumula_classificatoria=sumula, event=event)
+                players_score.append(PlayerScore.objects.create(
+                    player=player_obj, sumula_classificatoria=sumula, event=event))
+
+        return players_score
 
     def add_referees(self, sumula: SumulaImortal | SumulaClassificatoria, event: Event, referees: list) -> None:
         """Adiciona um árbitro a uma sumula."""
