698ad96633fe8fc8e4f1b800563a1fd058b87018
artrsousa1 <artrsousa1@gmail.com>
2024-09-30 16:49:14 -0300

    feat(front): updating results components

--- web/src/app/components/ResultsComponent.tsx
+++ web/src/app/components/ResultsComponent.tsx
@@ -1,44 +1,126 @@
-import React, { use, useContext } from "react";
+import React, { use, useContext, useEffect } from "react";
 import { useState } from "react";
 import DisplayComponent from "./DisplayPlayerComponent";
 import NoResults from "./NoResults";
 import { UserContext } from "../contexts/UserContext";
-import getResults from "../utils/api/getResults";
+import request from "@/app/utils/request";
+import { settingsWithAuth } from "@/app/utils/settingsWithAuth";
+import { usePathname } from "next/navigation";
+import capitalize from "@/app/utils/capitalize";
 
 export default function ResultsComponent() {
     const { user } = useContext(UserContext);
-    const [ isPlayer, setIsPlayer ] = useState<boolean>(false);
-    const [ result, setResult ] = useState<boolean>(false);
+    const [isPlayer, setIsPlayer] = useState<boolean>(false);
+    const [published, setPublished] = useState<boolean>(false);
+    const [results, setResults] = useState<any>();
+    const currentId = usePathname().split('/')[1];
+    const getEventResults = async () => {
 
-    return result ? <NoResults/> :
+    }
+
+    useEffect(() => {
+        getEventResults()
+    }, [user])
+
+    const playerMock = {
+        "id": 0,
+        "total_score": 32767,
+        "full_name": "string",
+        "social_name": "string"
+    }
+
+    const mock = {
+        "id": 5,
+        "top4": [
+            {
+                "id": 5,
+                "total_score": 98,
+                "full_name": "João da Silva",
+                "social_name": "João"
+            },
+            {
+                "id": 6,
+                "total_score": 98,
+                "full_name": "João da Silva",
+                "social_name": "João"
+            },
+            {
+                "id": 5,
+                "total_score": 98,
+                "full_name": "João da Silva",
+                "social_name": "João"
+            },
+            {
+                "id": 5,
+                "total_score": 98,
+                "full_name": "João da Silva",
+                "social_name": "João"
+            },
+        ],
+        "paladin": {
+            "id": 0,
+            "total_score": 98,
+            "full_name": "João da Silva",
+            "social_name": "João"
+        },
+        "ambassor": {
+            "id": 0,
+            "total_score": 98,
+            "full_name": "João da Silva",
+            "social_name": "João"
+        },
+        "imortals": [
+            {
+                "id": 5,
+                "total_score": 98,
+                "full_name": "João da Silva",
+                "social_name": "João"
+            },
+            {
+                "id": 5,
+                "total_score": 98,
+                "full_name": "João da Silva",
+                "social_name": "João"
+            },
+            {
+                "id": 5,
+                "total_score": 98,
+                "full_name": "João da Silva",
+                "social_name": "João"
+            },
+        ]
+    }
+
+    return published ? <NoResults /> :
         <>
             <div className="grid justify-center items-center gap-5 py-32">
-            {isPlayer && 
-                <div>
-                    <p className="font-semibold text-slate-700">SEU DESEMPENHO</p>
-                    <DisplayComponent playerName="Player 1" points={10}/>
-                </div>}
-            <div className="grid gap-3">
-                <p className="font-semibold text-slate-700">TOP 4</p>
-                <DisplayComponent playerName="Player 2" points={52}/>
-                <DisplayComponent playerName="Player 2" points={48}/>
-                <DisplayComponent playerName="Player 2" points={46}/>
-                <DisplayComponent playerName="Player 2" points={47}/>
+                {isPlayer &&
+                    <div>
+                        <p className="font-semibold text-slate-700">SEU DESEMPENHO</p>
+                        <DisplayComponent playerName={playerMock.full_name} points={playerMock.total_score} />
+                    </div>}
+                <div className="grid gap-3">
+                    <p className="font-semibold text-slate-700">TOP 4</p>
+                    {mock.top4 ? mock.top4.map((player, index) => {
+                        return <DisplayComponent key={index} playerName={capitalize(player.full_name)} points={player.total_score} />
+                    }) : <p> Ainda não divulgado</p>}
+                </div>
+                <div className="grid gap-3">
+                    <p className="font-semibold text-slate-700">IMORTAIS</p>
+                    {mock.imortals ? mock.imortals.map((player, index) => {
+                        return <DisplayComponent key={index} playerName={capitalize(player.full_name)} points={player.total_score}/>
+                    }): <p>Ainda não divulgado</p>}
+                </div>
+                <div className="grid gap-3">
+                    <p className="font-semibold text-slate-700">PALADINO</p>
+                    {mock.paladin ? <DisplayComponent playerName={mock.paladin.full_name} points={mock.paladin.total_score}/> :
+                    <p>Ainda não divulgado</p>}
+                </div>
+                <div className="grid gap-3">
+                    <p className="font-semibold text-slate-700">EMBAIXADOR</p>
+                    {mock.ambassor ? <DisplayComponent playerName={mock.ambassor.full_name} points={mock.ambassor.total_score}/> :
+                    <p>Ainda não divulgado</p>}
+                </div>
             </div>
-            <div className="grid gap-3">
-                <p className="font-semibold text-slate-700">IMORTAIS</p>
-                <DisplayComponent playerName="Player 2" points={90}/>
-                <DisplayComponent playerName="Player 2" points={89}/>
-                <DisplayComponent playerName="Player 2" points={88}/>
-            </div>
-            <div className="grid gap-3">
-                <p className="font-semibold text-slate-700">PALADINO</p>
-                <DisplayComponent playerName="Player 2" points={50}/>
-            </div>
-            
-
-        </div>
         </>
-        
-    
 };
