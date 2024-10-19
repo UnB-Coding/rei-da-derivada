eb9d4b8a7a2a66963fba009ff0b4b49f94a2208c
Gabriel Henrique Castelo <81991244+GabrielCastelo-31@users.noreply.github.com>
2024-10-18 22:52:03 -0300

    Front/create pwa (#153)
    * add pwa config
    
    * update manifest
    
    * updates depedencies
    
    * add config
    
    * update config
    
    * update
    
    * update thing
    
    * delete file
    
    * add git ignore

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -3,6 +3,12 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
     dest: "public",
+    register: true,
+    reloadOnOnline: true,
+    fallbacks: {
+        document: "src/app/~offline/page.tsx",
+    },
+    cacheOnFrontEndNav:true,
 });
 
 const nextConfig = {
